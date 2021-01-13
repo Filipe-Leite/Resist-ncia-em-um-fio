7e8cf48c8e644f4376858a4d5d45fe9a0be69fad
Be-hoot <45852466+Filipe-Leite@users.noreply.github.com>
2021-01-13 18:17:24 -0300

    correção do código
    Foi corrigido um erro de gráfico e melhorada uma função/

--- resistence_in_a_wire_graphic.py
+++ resistence_in_a_wire_graphic.py
@@ -19,77 +19,76 @@ def valores_comprimento_varia():
 
 def valores_area_varia():
     valoresdex = [0.01]
-    for i in range(149):
+    for i in range(166):
         novovalor = valoresdex[-1] + 0.09
         valoresdex.append(novovalor)
 
     return valoresdex
 
+def equacao_resistencia(valores, variacao):
+    if variacao == 'ro':
+
+        r = valores[0]*float(valores[1])/float(valores[2])
+
+    if variacao == 'l':
+
+        r = float(valores[0])*valores[1]/float(valores[2])
+
+    if variacao == 'a':
+
+        r = float(valores[0]) * float(valores[1]) / valores[2]
+
+    return r
+
 variacao=input('O que vai variar? ro / l / a \n\n')
 
 if variacao == 'ro':
 
-    nomedex    = "Resistividade"
-    nomedey    = "Resistência"
+    nomedex    = "Resistividade (Ωcm)"
+    nomedey    = "Resistência (ohms)"
     valoresdex = valores_resistividade_varia()
-    valoresdex_to_del = valoresdex[:]
     valoresder = []
-    l = float(input('Qual é o valor de L? \n \n'))
-    a = float(input('Qual é o valor de a? \n\n'))
+    l = input('Qual é o valor de L? \n \n')
+    a = input('Qual é o valor de a? \n\n')
 
-    while len(valoresdex_to_del) != 0:
+    for i in valoresdex:
 
-        valores = [valoresdex_to_del[0], l, a]
-
-        valores = valores
-        r = valores[0] * valores[1]
-        r = r/valores[2]
+        valores = [i, l, a]
+        r = equacao_resistencia(valores, variacao)
         valoresder.append(r)
-        del valoresdex_to_del[0]
 
 if variacao == 'l':
 
-    nomedex = "Comprimento"
-    nomedey = "Resistência"
+    nomedex = "Comprimento (cm)"
+    nomedey = "Resistência (ohms)"
     valoresdex = valores_comprimento_varia()
-    valoresdex_to_del = valoresdex[:]
     valoresder = []
-    ro = float(input('Qual é o valor de ro? \n \n'))
-    a = float(input('Qual é o valor de a? \n\n'))
+    ro = input('Qual é o valor de ro? \n \n')
+    a  = input('Qual é o valor de a? \n\n')
 
-    while len(valoresdex_to_del) != 0:
+    for i in valoresdex:
 
-        valores = [valoresdex_to_del[0], ro, a]
-
-        valores = valores
-        r = valores[0] * valores[1]
-        r = r/valores[2]
+        valores = [ro, i, a]
+        r = equacao_resistencia(valores, variacao)
         valoresder.append(r)
-        del valoresdex_to_del[0]
 
 if variacao == 'a':
 
-    nomedex = "Área"
-    nomedey = "Resistência"
+    nomedex = "Área (cm²)"
+    nomedey = "Resistência (ohms)"
     valoresdex = valores_area_varia()
-    valoresdex_to_del = valoresdex[:]
     valoresder = []
-    ro = float(input('Qual é o valor de ro? \n \n'))
-    l = float(input('Qual é o valor de l? \n\n'))
+    ro = input('Qual é o valor de ro? \n \n')
+    l  = input('Qual é o valor de l? \n\n')
 
-    while len(valoresdex_to_del) != 0:
+    for i in valoresdex:
 
-        valores = [valoresdex_to_del[0], ro, l]
-
-        valores = valores
-        r = valores[0] * valores[1]
-        r = r/valores[2]
+        valores = [ro, l, i]
+        r = equacao_resistencia(valores, variacao)
         valoresder.append(r)
-        del valoresdex_to_del[0]
 
-
-plt.plot(valoresdex, valoresder) #Criando o gráfico
-plt.title('Resistência em um fio') #adicionando o título
+plt.plot(valoresdex, valoresder) #criando o gráfico
+plt.title('Resistência em um fio\n(Resistence in a wire)') #adicionando o título
 plt.xlabel(nomedex)
 plt.ylabel(nomedey)
 plt.show()
